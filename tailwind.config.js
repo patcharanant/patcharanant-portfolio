6673e597e02b1b5410266b362842016ceb0b749c
Patcharanant Boonyawisitpokin <patcharanant.b@gmail.com>
2022-01-21 01:59:39 +0700

    - init project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'main-light': '#6e57e0',
+      },
+    },
+  },
+  plugins: [],
+}
